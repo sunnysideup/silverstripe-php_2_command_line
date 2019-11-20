e5a4424b078c10839968c79e8900be7405dacbdc
thiemen <nt@sunnysideup.co.nz>
2019-11-21 11:04:13 +1300

    PATCH: create log folder

--- src/PHP2CommandLineSingleton.php
+++ src/PHP2CommandLineSingleton.php
@@ -372,6 +372,8 @@ class PHP2CommandLineSingleton
     protected function writeToFile($fileName, $data, $newLineCount)
     {
         if (! file_exists($fileName)) {
+            $folder = dirname($fileName);
+            mkdir($folder, 0777, true);
             file_put_contents($fileName, date('Y-m-d h:i'));
             file_put_contents($fileName, PHP_EOL.PHP_EOL, FILE_APPEND | LOCK_EX);
         } else {
